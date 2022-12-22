2ddb5704769748b072f6361ff7ddd1061f876a92
crfmc <crf85@cornell.edu>
2022-12-21 20:23:26 -0500

    - Separate last name - Substitute non-breaking hyphen

--- client/src/v2/Greeting.js
+++ client/src/v2/Greeting.js
@@ -13,11 +13,11 @@ function Greeting(props)
         <SideLinks />
       </div>
       <h2 className={styles.hello}>Hello, my name is</h2>
-      <h1 className={styles.name}>Cesar Ferreyra-Mansilla.</h1>
+      <h1 className={styles.name}>Cesar <span className={styles.last_name}>Ferreyra-Mansilla.</span></h1>
       <h2 className={styles.subtitle}>I am a developer for the web.</h2>
       {props.topNav && 
         <div className={styles.subtext_container}>
-          <p className={styles.subtext}>My focus is on building effective goal-driven user experiences for websites.</p>
+          <p className={styles.subtext}>My focus is on building effective goal&#8209;driven user experiences for websites.</p>
           <a href="mailto:crf85@cornell.edu" className={styles.subtext_link}>Email me</a>
         </div>
       }
